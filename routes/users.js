a5be965d9057d973ceed874f556e2d008e3cb5f1
Laura Baukol <laura.baukol@mac.com>
2016-12-01 14:52:26 -0700

    part 3 passed

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,36 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const {
+    camelizeKeys,
+    decamelizeKeys
+} = require('humps');
 
-// YOUR CODE HERE
+router.post('/users', (req, res, next) => {
+    var saltRounds = 12;
+    var hash = bcrypt.hashSync(req.body.password, saltRounds);
+    knex('users')
+        .insert({
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            email: req.body.email,
+            hashed_password: hash
+        }, '*')
+        .then((rows) => {
+            var users = camelizeKeys(rows[0]);
+            console.log(users);
+            delete users.hashedPassword;
+            res.send(users);
+            console.log(users);
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
 
 module.exports = router;
